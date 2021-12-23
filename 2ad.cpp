41cc5ff28937a43a9353476a455cc2758e0044e2
Yaroslav <95548740+eternalxijaderod@users.noreply.github.com>
2021-12-23 23:35:27 +0200

    Update 2ad.cpp

--- 2ad.cpp
+++ 2ad.cpp
@@ -1,69 +1,76 @@
 #include <iostream>
-int KakoyDenNedeli0101(int year)
+using namespace std;
+
+
+int main(){
+int day[31],month[12];
+int year[101];
+int t;
+
+cin>>t;
+
+
+for (int i=0; i < t; i++)
+    {
+        cin>>day[i]>>month[i]>>year[i];
+    }
+
+for (int i=0; i < t;i++)
 {
-    short a = year%28;
-    if(a == 2 || a == 13 || a==7 || a == 8)
-        return 0;
-    else if (a == 25 || a == 3 || a == 14 || a == 20)
-        return 1;
-    else if (a == 26 || a == 9 || a == 15 || a == 4)
-        return 2;
-    else if (a == 27 || a == 10 || a == 21 || a == 16)
-        return 3;
-    else if (a == 5 || a == 11 || a == 22 || a == 0)
-        return 4;
-    else if (a == 6 || a == 17 || a == 23 || a == 12)
-        return 5;
-    else
-        return 6;
+	if (year[i] < 2016 &&  year[i]%4==0  && month[i]<3)
+	{cout<<endl<<year[i]+28;}
+
+	if (year[i] == 2000 &&  year[i]%4==0  && month[i]>2)
+	{cout<<endl<<year[i]+17;}
+
+	if (year[i] == 2004 &&  year[i]%4==0  && month[i]>2)
+	{cout<<endl<<year[i]+17;}
+
+	if (year[i] == 2008 &&  year[i]%4==0  && month[i]>2)
+	{cout<<endl<<year[i]+17;}
+
+	if (year[i] == 2012 &&  year[i]%4==0  && month[i]>2)
+	{cout<<endl<<year[i]+6;}
+
+
+
+    if (year[i] > 2015 )
+	{cout<<endl<<year[i];}
+
+if (year[i]==2001)
+	{cout<<endl<<2018;}
+if (year[i]==2002)
+	{cout<<endl<<2019;}
+if (year[i]==2003)
+	{cout<<endl<<2025;}
+if (year[i]==2005)
+	{cout<<endl<<2022;}
+if (year[i]==2006)
+	{cout<<endl<<2017;}
+if (year[i]==2007)
+	{cout<<endl<<2018;}
+if (year[i]==2009)
+	{cout<<endl<<2026;}
+if (year[i]==2010)
+	{cout<<endl<<2021;}
+
+if (year[i]==2011&& month[i]<3)
+	{cout<<endl<<2022;}
+
+if (year[i]==2011&& month[i]>2)
+	{cout<<endl<<2016;}
+if (year[i]==2013)
+	{cout<<endl<<2019;}
+if (year[i]==2014)
+	{cout<<endl<<2025;}
+if (year[i]==2015&& month[i]<3)
+	{cout<<endl<<2028;}
+if (year[i]==2015 && month[i]>2)
+	{cout<<endl<<2020;}
+
 }
 
-bool VysokosLi(int year)
-{
-    if(year%400 == 0 || year % 4 == 0 && year % 100 !=0)
-    {
-        //std::cout << "Год високосный" << std::endl;
-        return 1;
-    }
-    else
-    {
-        //std::cout << "Год невисокосный" << std::endl;
-        return 0;
-    }
-}
 
-int main() {
-    //system("chcp 65001");
-    int year = 0;
-    int skolkoDneyVGodu;
-    int nomerDnya;
-    std::cin >> year >> nomerDnya;
-    int denNedeli0101 = KakoyDenNedeli0101(year);
-    int denNedeliNuzhniy;
-
-
-    bool vysokosLi = VysokosLi(year);
-    if(vysokosLi == 1)
-        skolkoDneyVGodu = 366;
-    else
-        skolkoDneyVGodu = 365;
-    //std::cout << "В " << year << " году " << skolkoDneyVGodu << " дней" << std::endl;
-
-
-
-        for (int i = year+1; ;i++)
-        {
-            if(vysokosLi == VysokosLi(i) && denNedeli0101 == KakoyDenNedeli0101(i))
-            {
-                std::cout << i << std::endl;
-                break;
-            }
-        }
-        //if (nomerDnya >= 60)
-        //    denNedeliNuzhniy = nomerDnya % 7 + denNedeli0101 + vysokosLi;
-        //else
-        //    denNedeliNuzhniy = nomerDnya % 7 + denNedeli0101;
-        //std::cout << denNedeliNuzhniy << std::endl;
-    return 0;
+return 0;
 
 }
